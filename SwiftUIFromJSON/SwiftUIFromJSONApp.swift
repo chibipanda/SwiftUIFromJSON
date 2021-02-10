7acc5b73d903a570514731a4e12b7b8890cb83b7
Angelina Wu <babylemon@gmail.com>
2021-02-10 18:41:39 -0500

    Initial Commit

---
+++ SwiftUIFromJSON/SwiftUIFromJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFromJSONApp.swift
+//  SwiftUIFromJSON
+//
+//  Created by Angelina Wu on 10/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFromJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
